4b90f2b9a5fc97de16960e68835880c7b6abe09b
jessehu312 <jessehu312@gmail.com>
2021-02-20 11:54:39 -0800

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { useAuth } from '@/lib/auth';
+import Head from 'next/head';
+
+export default function Home() {
+  const auth = useAuth();
+  const { user, email } = auth;
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-3xl font-bold text-blue-500">InvestX Landing Page</h1>
+      <p>{user ? user?.email : 'Logged out'}</p>
+      <button
+        onClick={(e) => auth.signInWithGoogle()}
+        className="bg-blue-600 text-white"
+      >
+        Sign In With Google
+      </button>
+      <button
+        onClick={(e) => auth.signInWithTwitter()}
+        className="block bg-green-600 text-white mt-4"
+      >
+        Sign In With Twitter
+      </button>
+    </div>
+  );
+}
