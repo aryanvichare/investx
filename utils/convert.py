5f598bed9b33014a04eca339c7a81c43dbe3ec5d
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2021-02-20 23:01:26 -0800

    Fix News API and conversion (#9)

--- utils/convert.py
+++ utils/convert.py
@@ -24,25 +24,20 @@ NY = 'America/New_York'
 start=pd.Timestamp('2021-02-19', tz=NY).isoformat()
 end=pd.Timestamp('2021-02-19', tz=NY).isoformat()
 
-llama_limit = []
-for i in range(100):
-    llama_limit.append(sp500.popleft())
-
-stocks = alpaca.get_barset(llama_limit, 'day', start=start, end=end)
+all_stocks = {}
+while sp500:
+    llama_limit = [sp500.popleft() for _ in range(100) if sp500]
+    stocks = alpaca.get_barset(llama_limit, 'day', start=start, end=end)
+    while len(stocks):
+        item = stocks.popitem()
+        bar = item[1].pop()
+        all_stocks[item[0]] = { 'price': bar.c, 'change': str(round(bar.o - bar.c, 2)) }
 
 csvfile = open('utils/sp500.csv', 'r')
 reader = csv.DictReader( csvfile, fieldnames)
+jsonfile.write('[')
 for row in reader:
-    item = stocks.popitem()
-    bar = item[1].pop()
-    print(item[0], bar.c, bar.o - bar.c)
-    row['price'] = bar.o
-    row['change'] = str(round(bar.o - bar.c, 2))
+    row.update(all_stocks[row['symbol']])
     json.dump(row, jsonfile)
     jsonfile.write(',\n')
-
-    if not len(stocks) and sp500:
-        llama_limit = []
-        for i in range(min(100, len(sp500))):
-            llama_limit.append(sp500.popleft())
-        stocks = alpaca.get_barset(llama_limit, 'day', start=start, end=end)
+jsonfile.write(']\n')
