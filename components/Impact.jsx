00dbd6ffb4472991fe99d8a67ed8e2a3a8e67098
jessehu312 <64887515+jessehu312@users.noreply.github.com>
2021-02-21 02:43:02 -0800

    Refactor components (#13)

--- components/Impact.jsx
+++ components/Impact.jsx
@@ -33,8 +33,8 @@ const Impact = () => {
               key={idx}
               className="group bg-white shadow-lg rounded-lg p-12 border transform hover:scale-105 transition ease-in-out duration-300 hover:bg-blue-500"
             >
-              <img class="w-36 mx-auto" src={`/images/${el.img}`} alt="City" />
-              <h2 class="text-blue-500 group-hover:text-white text-2xl font-bold text-center mt-4">
+              <img className="w-36 mx-auto" src={`/images/${el.img}`} alt="City" />
+              <h2 className="text-blue-500 group-hover:text-white text-2xl font-bold text-center mt-4">
                 {el.name}
               </h2>
               <p className="text-sm text-center leading-5 text-gray-500 group-hover:text-white mt-2">
