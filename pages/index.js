59e1d3e10c89d62df22c27ebc25fad35a99c539e
jessehu312 <jessehu312@gmail.com>
2021-02-21 02:28:11 -0800

    Refactor components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Header from '@/components/Header';
+import Header from '@/components/home/Header';
 import Impact from '@/components/Impact';
-import Banner from '@/components/Banner';
-import Footer from '@/components/Footer';
+import Banner from '@/components/home/Banner';
+import Footer from '@/components/home/Footer';
 
 export default function Home() {
   return (
